cdb32dccabec55f2bf91549e59b514bd19f4f2e1
John Krasnay <john@krasnay.ca>
2015-05-08 14:46:00 -0400

    Implement TextAreaControl

---
+++ src/main/java/ca/krasnay/panelized/TextAreaControl.java
@@ -0,0 +1,80 @@
+package ca.krasnay.panelized;
+
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.form.FormComponent;
+import org.apache.wicket.markup.html.form.TextArea;
+import org.apache.wicket.model.AbstractReadOnlyModel;
+import org.apache.wicket.model.IModel;
+
+/**
+ * Form item panel that encapsulates a text area.
+ *
+ * @author John Krasnay <john@krasnay.ca>
+ */
+public class TextAreaControl extends AbstractControl<String> {
+
+    private boolean required;
+
+    private int rows = 6;
+
+    private TextArea<String> textArea;
+
+    public TextAreaControl(String id) {
+        this(id, null);
+    }
+
+    public TextAreaControl(String id, IModel<String> model) {
+
+        super(id, model);
+
+        textArea = new TextArea<String>("field", new DelegateModel<String>(this)) {
+            @Override
+            public boolean isRequired() {
+                return TextAreaControl.this.isRequired();
+            }
+        };
+
+        add(textArea);
+
+        textArea.setType(String.class);
+        textArea.setOutputMarkupId(true);
+
+        textArea.add(new AttributeModifier("rows", new AbstractReadOnlyModel<String>() {
+            @Override
+            public String getObject() {
+                return "" + rows;
+            }
+        }));
+
+    }
+
+    public TextArea<String> getTextArea() {
+        return textArea;
+    }
+
+    @Override
+    public boolean isRequired() {
+        return required;
+    }
+
+    public TextAreaControl setLabel(IModel<String> label) {
+        textArea.setLabel(label);
+        return this;
+    }
+
+    @Override
+    public TextAreaControl setRequired(boolean required) {
+        this.required = required;
+        return this;
+    }
+
+    @Override
+    public FormComponent<String> getFormComponent() {
+        return textArea;
+    }
+
+    public TextAreaControl setRows(int rows) {
+        this.rows = rows;
+        return this;
+    }
+}
--- src/test/java/ca/krasnay/panelized/testapp/TestDialog.java
+++ src/test/java/ca/krasnay/panelized/testapp/TestDialog.java
@@ -1,10 +1,13 @@
 package ca.krasnay.panelized.testapp;
 
 import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.model.Model;
 
 import ca.krasnay.panelized.AjaxAction;
 import ca.krasnay.panelized.DialogPanel;
-import ca.krasnay.panelized.TextPanel;
+import ca.krasnay.panelized.LabelledPanel;
+import ca.krasnay.panelized.TextAreaControl;
+import ca.krasnay.panelized.TextFieldControl;
 
 public class TestDialog extends DialogPanel {
 
@@ -16,7 +19,14 @@ public class TestDialog extends DialogPanel {
 
         DialogFrame frame = buildFrame(title);
 
-        frame.addPanel(new TextPanel(frame.newPanelId(), "Hello, world"));
+//        frame.addPanel(new TipPanel(frame.newPanelId(), "This is a test"));
+
+        frame.addPanel(new LabelledPanel(frame.newPanelId(), "Name")
+        .addPanel(new TextFieldControl("name", Model.of(""))
+        .setRequired(true)));
+
+        frame.addPanel(new LabelledPanel(frame.newPanelId(), "Life Story")
+        .addPanel(new TextAreaControl("name", Model.of(""))));
 
         frame.addSaveButton(saveAction);
         frame.addCancelButton();
