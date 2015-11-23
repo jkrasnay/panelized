ffc2165ff662b2431cc74a0e4759e6f414339ab3
John Krasnay <john@krasnay.ca>
2015-11-23 13:24:19 -0500

    Make PageTitle annotation localizable

--- src/main/java/ca/krasnay/panelized/BookmarkablePageLinkPanel.java
+++ src/main/java/ca/krasnay/panelized/BookmarkablePageLinkPanel.java
@@ -4,19 +4,31 @@ import org.apache.wicket.markup.html.basic.Label;
 import org.apache.wicket.markup.html.link.BookmarkablePageLink;
 import org.apache.wicket.markup.html.panel.Panel;
 import org.apache.wicket.model.IModel;
-import org.apache.wicket.model.Model;
+import org.apache.wicket.model.ResourceModel;
 
+/**
+ * Panel containing a link to a bookmarkable page.
+ *
+ * @author <a href="mailto:john@krasnay.ca">John Krasnay</a>
+ */
 public class BookmarkablePageLinkPanel extends Panel {
 
     private static IModel<String> getDefaultPageTitle(PageRef pageRef) {
 
         if (pageRef.getPageClass().isAnnotationPresent(PageTitle.class)) {
-            return Model.of(pageRef.getPageClass().getAnnotation(PageTitle.class).value());
+            String value = pageRef.getPageClass().getAnnotation(PageTitle.class).value();
+            return new ResourceModel(value, value);
         } else {
             throw new RuntimeException("Page " + pageRef.getPageClass().getName() + " does not have a @PageTitle annotation");
         }
     }
 
+    /**
+     * Constructor that uses the page's {@link PageTitle} annotation to determine
+     * link text. The value of the {@link PageTitle} annotation is used to look
+     * up a string resource with that key. If not found, the value itself is
+     * displayed, so the value can contain the page title directly for monolingual apps.
+     */
     public BookmarkablePageLinkPanel(String panelId, PageRef pageRef) {
         this(panelId, getDefaultPageTitle(pageRef), pageRef);
     }
